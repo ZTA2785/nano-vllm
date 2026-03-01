4dce954a55550bea1134172e3df439848b77d95e
ZTA20040910 <zta2785@163.com>
2026-03-01 15:00:19 +0800

    add

--- example.py
+++ example.py
@@ -4,11 +4,17 @@ from transformers import AutoTokenizer
 
 
 def main():
+    # 确定模型路径：
     path = os.path.expanduser("~/huggingface/Qwen3-0.6B/")
+    
+    # 加载 model 和 tokenizer：
     tokenizer = AutoTokenizer.from_pretrained(path)
-    llm = LLM(path, enforce_eager=True, tensor_parallel_size=1)
-
+    llm = LLM(path, enforce_eager=True, tensor_parallel_size=1) # LLM 就是 LLMEngine 的一层名字上的包装
+    
+    # 确定采样参数：
     sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
+    
+    # 准备 prompts：
     prompts = [
         "introduce yourself",
         "list all prime numbers within 100",
@@ -21,6 +27,8 @@ def main():
         )
         for prompt in prompts
     ]
+    
+    # 推理：
     outputs = llm.generate(prompts, sampling_params)
 
     for prompt, output in zip(prompts, outputs):
